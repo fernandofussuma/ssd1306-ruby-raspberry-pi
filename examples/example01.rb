1a672145e8e7db97833f2ab2be39f29b8a8562da
Fernando Fussuma <fernandofussuma@gmail.com>
2015-02-04 00:42:33 -0200

    spi test

--- examples/example01.rb
+++ examples/example01.rb
@@ -39,10 +39,7 @@ SSD1306_VERTICAL_AND_LEFT_HORIZONTAL_SCROLL = 0x2A
 reset = PiPiper::Pin.new(:pin => 24, :direction => :out)
 dc = PiPiper::Pin.new(:pin => 23, :direction => :out)
 
-reset.on
-dc.on
-
-def reset_ssd1306
+def reset_ssd1306(reset)
   reset.on
   sleep(0.001)
   reset.off
@@ -50,7 +47,7 @@ def reset_ssd1306
   reset.on
 end
 
-def command(data)
+def command(dc, data)
   dc.off
   PiPiper::Spi.begin do
     write data
@@ -59,34 +56,34 @@ end
 
 def start()
     # 128x64 pixel specific initialization.
-    command(SSD1306_DISPLAYOFF)                    # 0xAE
-    command(SSD1306_SETDISPLAYCLOCKDIV)            # 0xD5
-    command(0x80)                                  # the suggested ratio 0x80
-    command(SSD1306_SETMULTIPLEX)                  # 0xA8
-    command(0x3F)
-    command(SSD1306_SETDISPLAYOFFSET)              # 0xD3
-    command(0x0)                                   # no offset
-    command(SSD1306_SETSTARTLINE | 0x0)            # line #0
-    command(SSD1306_CHARGEPUMP)                    # 0x8D
-    command(0x14)
-    command(SSD1306_MEMORYMODE)                    # 0x20
-    command(0x00)                                  # 0x0 act like ks0108
-    command(SSD1306_SEGREMAP | 0x1)
-    command(SSD1306_COMSCANDEC)
-    command(SSD1306_SETCOMPINS)                    # 0xDA
-    command(0x12)
-    command(SSD1306_SETCONTRAST)                   # 0x81
-    command(0xCF)
-    command(SSD1306_SETPRECHARGE)                  # 0xd9
-    command(0xF1)
-    command(SSD1306_SETVCOMDETECT)                 # 0xDB
-    command(0x40)
-    command(SSD1306_DISPLAYALLON_RESUME)           # 0xA4
-    command(SSD1306_NORMALDISPLAY)                 # 0xA6
+    command(dc, SSD1306_DISPLAYOFF)                    # 0xAE
+    command(dc, SSD1306_SETDISPLAYCLOCKDIV)            # 0xD5
+    command(dc, 0x80)                                  # the suggested ratio 0x80
+    command(dc, SSD1306_SETMULTIPLEX)                  # 0xA8
+    command(dc, 0x3F)
+    command(dc, SSD1306_SETDISPLAYOFFSET)              # 0xD3
+    command(dc, 0x0)                                   # no offset
+    command(dc, SSD1306_SETSTARTLINE | 0x0)            # line #0
+    command(dc, SSD1306_CHARGEPUMP)                    # 0x8D
+    command(dc, 0x14)
+    command(dc, SSD1306_MEMORYMODE)                    # 0x20
+    command(dc, 0x00)                                  # 0x0 act like ks0108
+    command(dc, SSD1306_SEGREMAP | 0x1)
+    command(dc, SSD1306_COMSCANDEC)
+    command(dc, SSD1306_SETCOMPINS)                    # 0xDA
+    command(dc, 0x12)
+    command(dc, SSD1306_SETCONTRAST)                   # 0x81
+    command(dc, 0xCF)
+    command(dc, SSD1306_SETPRECHARGE)                  # 0xd9
+    command(dc, 0xF1)
+    command(dc, SSD1306_SETVCOMDETECT)                 # 0xDB
+    command(dc, 0x40)
+    command(dc, SSD1306_DISPLAYALLON_RESUME)           # 0xA4
+    command(dc, SSD1306_NORMALDISPLAY)                 # 0xA6
 end
 
 while true
-  reset_ssd1306()
+  reset_ssd1306(reset)
   start()
-  command(SSD1306_DISPLAYON)
+  command(dc, SSD1306_DISPLAYON)
 end
