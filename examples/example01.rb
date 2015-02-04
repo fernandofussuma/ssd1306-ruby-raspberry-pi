4e896e0bcb84508cc293f272d6059263c4161229
Fernando Fussuma <fernandofussuma@gmail.com>
2015-02-04 00:08:18 -0200

    spi test

--- examples/example01.rb
+++ examples/example01.rb
@@ -1,10 +1,37 @@
 require 'pi_piper'
 
-pin = PiPiper::Pin.new(:pin => 24, :direction => :out)
+reset = PiPiper::Pin.new(:pin => 24, :direction => :out)
+dc = PiPiper::Pin.new(:pin => 23, :direction => :out)
 
-while (true)
-  pin.on
-  sleep(100)
-  pin.off
-  sleep(100)
+reset.off
+dc.off
+
+reset.on
+sleep(0.001)
+reset.off
+sleep(0.01)
+reset.on
+
+dc.off
+
+# """Reset the display."""
+# # Set reset high for a millisecond.
+# self._gpio.set_high(self._rst)
+# time.sleep(0.001)
+# # Set reset low for 10 milliseconds.
+# self._gpio.set_low(self._rst)
+# time.sleep(0.010)
+# # Set reset high again.
+# self._gpio.set_high(self._rst)
+
+
+
+PiPiper::Spi.begin do
+  write 0xAE
+  write 0xD5
+  write 0x80
+  write 0x3F
+  write 0xD3
+  write 0x00
+  write 0xAE
 end
